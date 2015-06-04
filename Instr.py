f0d8983e5f73069b4e557a080ba060763ccfd097
Chris Marquez <chrismarquez329@gmail.com>
2015-06-04 00:30:58 -0500

    Revamped Project

---
+++ Instr.py
@@ -0,0 +1,16 @@
+'''
+Instr.py
+
+Instr class to handle wimp51 instruction validation and associated tasks
+'''
+
+#!/usr/bin/python -i
+
+class Instr:
+
+    INSTR_SET = ['MOV', 'ADDC', 'START', 'END']
+
+    def __init__ (self):
+        pass
+    
+
--- Program.py
+++ Program.py
@@ -1,108 +1,117 @@
-#!/usr/bin/python3 -i
+'''
+Program.py
 
-import sys
+Program class to handle wimp51 'assembly' source code and convert it to binary
+and/or hex output files for easy input into the Altera Cyclone II.
+'''
+
+#!/usr/bin/python -i
+
+use Instr
 
 class Program:
 
-    def __init__ (self, src_filename, out_filename="wimp.bin"):
+    # Program Class Constructor
+    def __init__ (self, input_file, output_file):
 
-        self.src_filename = src_filename 
-        self.out_filename = out_filename 
-
-        # Open Input and Output file handlers
-        try:
-            self.src_f = open(self.src_filename, 'r') 
-            self.out_f    = open(self.out_filename, 'w')
-        except IOError as e:
-            print ("Error ({0}): '{1}': {2}".format(e.errno, \
-                                                    self.src_filename, \
-                                                    e.strerror))
-
-        self.src_code = self.get_src_code()
-        self.instr = []
+        self.src = []
         self.comments = []
-        self.get_instr()
-
+        self.instructions = []
+        self.num_comments = len (self.comments)
+        self.num_instructions = len (self.instructions)
+        self.byte_size = None
         
-        # Program Information
-        self.prog_size = self.get_prog_size 
-        #self.num_comments = len(self.comments)
-        #self.num_instructions = len(self.instr)
-        #self.num_src_lines = len(self.src_code) # Number of Non-blank lines
+        self.input_file = input_file
+        self.input_fh = None
 
-        # Close Input and Output file handlers
+        self.output_file = output_file
+        self.output_fh = None
+        
+        self.source_lines = None
+
+    def init_files (self):
+        fail = 0
+        
         try:
-            self.src_f.close()
-            self.out_f.close()
-
+            self.input_fh = open (self.input_file, 'r')
         except IOError as e:
-            print ("Error ({0}): '{1}': {2}".format(e.errno, \
-                                                    self.out_f, \
-                                                    e.strerror))
+            print ("Error ({0}). Couln't Open {1}...{2}").format(e.errno,
+                                                                 self.input_file,
+                                                                 e.strerror)
+            fail = 1
 
-    # Return list of non-empty lines in source code
-    def get_src_code (self):
-        
-        lines = [line.rstrip() for line in self.src_f] 
-        return [line for line in lines if line] 
+        try:
+            self.output_fh = open (self.output_file, 'w')
+        except IOError as e:
+            print ("Error ({0}). Couln't Open {1}...{2}").format(e.errno,
+                                                                 self.output_file,
+                                                                 e.strerror)
+            fail = 1
 
-    # Return list of (split) lines with valid commands
-    def get_instr (self):
-        for line in self.src_code:
-            i = Instruction(line)
-            if i.valid_cmd():
-                i.src_line = [x.rstrip(',') for x in i.src_line]
-                self.instr.append(i)
+        return 1 if not fail else 0
 
-    def get_comments (self):
-        pass
+    # Get program source
+    def get_src (self):
 
-    # Get program size in bytes
-    def get_prog_size (self):
-        pass
+        # Preliminary Line Formatting
+        tmp = map (lambda x: x.rstrip(), self.input_fh)
+        tmp = filter (lambda x: x != '', tmp) 
 
-    # String Representation of Program
+         # Split lines
+        tmp = map (lambda x: x.split(' '), tmp)
+
+        # More Formatting
+        for line in tmp:
+            line = map (lambda x: x.rstrip(','), line)
+            self.src.append(line)
+
+    # Instance Representative String
     def __str__ (self):
-        return "Assemble \'" + self.src_filename + "\'; Output: \'" + \
-               self.out_filename + "\'"
+        pass
 
-    # Comparison based on program size (bytes)
-    def __lt__ (self, other):
-        return self.prog_size < other.prog_size
+    # Rich Comparison based on program size (in bytes)
+    def __lt__ (self, prog2):
+        try:
+            return self.byte_size < prog2.byte_size
+        except (AttributeError, TypeError):
+            return False
 
-    def __le__ (self, other):
-        return self.prog_size <= other.prog_size
-    
-    def __eq__ (self, other):
-        return self.prog_size == other.prog_size
-    
-    def __ne__ (self, other):
-        return self.prog_size != other.prog_size
+    def __le__ (self, prog2):
+        try:
+            return self.byte_size <= prog2.byte_size
+        except (AttributeError, TypeError):
+            return False
+
+    def __eq__ (self, prog2):
+        try:
+            return self.byte_size == prog2.byte_size
+        except (AttributeError, TypeError):
+            return False
         
-    def __gt__ (self, other):
-        return self.prog_size > other.prog_size
+    def __ne__ (self, prog2):
+        try:
+            return self.byte_size <= prog2.byte_size
+        except (AttributeError, TypeError):
+            return False
 
-    def __ge__ (self, other):
-        return self.prog_size >= other.prog_size
+    def __gt__ (self, prog2):
+        try:
+            return self.byte_size > prog2.byte_size
+        except (AttributeError, TypeError):
+            return False
 
-class Instruction:
+    def __ge__ (self, prog2):
+        try:
+            return self.byte_size >= prog2.byte_size
+        except (AttributeErrror, TypeError):
+            return False
 
-    INSTR_SET = ['MOV', 'ADDC', 'ORL', 'ANL', 'XRL', \
-                 'SWAP', 'CLR', 'SETB', 'SJMP', 'JZ', 'LCD']
+def main ():
+    prog = Program ("input", "output")
 
-    REGISTERS = ['R0', 'R1', 'R2', 'R3', 'R4', \
-                 'R5', 'R6', 'R7', 'R8', 'R9']
+    if prog.init_files ():
+        prog.get_src ()
     
-    def __init__ (self, src_line):
-        self.src_line = src_line.split()
+    print (prog.src)
 
-    def valid_cmd (self):
-        return True if self.src_line[0] in Instruction.INSTR_SET else False
-        
-def main():
-
-    p = Program('in.wimp')
-    
-    for i in p.instr:
-        print (i.src_line)
-
+    print Instr.INSTR_SET
